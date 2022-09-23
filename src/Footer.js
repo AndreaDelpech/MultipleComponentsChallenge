ae7eddda0c1f529c9076439730c5a0616620bde8
Andrea Delpech <delpech.andrea@gmail.com>
2022-09-23 14:36:02 -0700

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+  return <div>Contact us to add more cities</div>;
+}
---
+++ src/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header() {
+  return <div>The list of cities is:</div>;
+}
